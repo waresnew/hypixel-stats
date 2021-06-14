f32eb002ede808c1b3d4974c3d3f73bfca590172
newwares <newwares123@gmail.com>
2021-06-14 11:35:32 -0400

    fixed duplicates

--- src/main/java/com/newwares/hypixelstats/utils/StatDisplayUtils.java
+++ src/main/java/com/newwares/hypixelstats/utils/StatDisplayUtils.java
@@ -5,49 +5,58 @@ import com.newwares.hypixelstats.api.modes.Player;
 import com.newwares.hypixelstats.config.ConfigData;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class StatDisplayUtils {
+    private static final ArrayList<String> playerList = new ArrayList<>();
+
     public static void stat(String gametype, String mode, String uuid, String username, boolean join) throws IOException, InterruptedException {
-
-        switch (gametype) {
-            case "BEDWARS": {
-                if (ConfigData.getInstance().isEnabledBedwars()) {
-                    StatDisplayUtils.printStats(new HypixelApi("BEDWARS", uuid.replace("-", ""), username).setStats(), join);
-                }
-                break;
-            }
-
-            case "SPEED_UHC": {
-                if (ConfigData.getInstance().isEnabledSpeedUhc()) {
-                    StatDisplayUtils.printStats(new HypixelApi("SPEED_UHC", uuid.replace("-", ""), username).setStats(), join);
-                }
-                break;
-            }
-
-            case "SKYWARS": {
-                switch (mode) {
-                    case "ranked_normal": {
-                        if (ConfigData.getInstance().isEnabledRankedSkywars()) {
-                            StatDisplayUtils.printStats(new HypixelApi("RANKED_SKYWARS", uuid.replace("-", ""), username).setStats(), join);
-                        }
-                        break;
+        if (!playerList.contains(uuid)) {
+            switch (gametype) {
+                case "BEDWARS": {
+                    if (ConfigData.getInstance().isEnabledBedwars()) {
+                        StatDisplayUtils.printStats(new HypixelApi("BEDWARS", uuid.replace("-", ""), username).setStats(), join);
+                        playerList.add(uuid);
                     }
-                    case "teams_normal":
-                    case "solo_normal": {
-                        if (ConfigData.getInstance().isEnabledNormalSkywars()) {
-                            StatDisplayUtils.printStats(new HypixelApi("NORMAL_SKYWARS", uuid.replace("-", ""), username).setStats(), join);
-                        }
-                        break;
-                    }
-                    case "teams_insane":
-                    case "solo_insane": {
-                        if (ConfigData.getInstance().isEnabledInsaneSkywars()) {
-                            StatDisplayUtils.printStats(new HypixelApi("INSANE_SKYWARS", uuid.replace("-", ""), username).setStats(), join);
-                        }
-                        break;
-                    }
+                    break;
                 }
-                break;
+
+                case "SPEED_UHC": {
+                    if (ConfigData.getInstance().isEnabledSpeedUhc()) {
+                        StatDisplayUtils.printStats(new HypixelApi("SPEED_UHC", uuid.replace("-", ""), username).setStats(), join);
+                        playerList.add(uuid);
+                    }
+                    break;
+                }
+
+                case "SKYWARS": {
+                    switch (mode) {
+                        case "ranked_normal": {
+                            if (ConfigData.getInstance().isEnabledRankedSkywars()) {
+                                StatDisplayUtils.printStats(new HypixelApi("RANKED_SKYWARS", uuid.replace("-", ""), username).setStats(), join);
+                                playerList.add(uuid);
+                            }
+                            break;
+                        }
+                        case "teams_normal":
+                        case "solo_normal": {
+                            if (ConfigData.getInstance().isEnabledNormalSkywars()) {
+                                StatDisplayUtils.printStats(new HypixelApi("NORMAL_SKYWARS", uuid.replace("-", ""), username).setStats(), join);
+                                playerList.add(uuid);
+                            }
+                            break;
+                        }
+                        case "teams_insane":
+                        case "solo_insane": {
+                            if (ConfigData.getInstance().isEnabledInsaneSkywars()) {
+                                StatDisplayUtils.printStats(new HypixelApi("INSANE_SKYWARS", uuid.replace("-", ""), username).setStats(), join);
+                                playerList.add(uuid);
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                }
             }
         }
     }
